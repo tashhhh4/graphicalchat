53e3c00eff52816fc0536a404d4b4a0b83943836
Natasha Libera <tash@artoftash.com>
2024-03-19 11:22:19 +0100

    still

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -1,7 +1,11 @@
 from config.settings.common import *
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/static/",
+]
+STATIC_URL = "/staticfiles/"
 
 # Allowed Hosts
 ALLOWED_HOSTS = ['graphicalchat.app', '70.34.203.236']
