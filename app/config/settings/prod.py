b41e38257fd09f8aac2aa3a1cb05ea3fd4c34e5a
Natasha Libera <tash@artoftash.com>
2024-03-19 11:34:57 +0100

    Tried some fixes

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -1,11 +1,12 @@
 from config.settings.common import *
 
+# Static Files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     "/static/",
 ]
-STATIC_URL = "/staticfiles/"
+STATIC_URL = "/static/"
 
 # Allowed Hosts
 ALLOWED_HOSTS = ['graphicalchat.app', '70.34.203.236']
