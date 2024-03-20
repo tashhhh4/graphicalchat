63261c9488a42e5fdb40de8fce6d6d472c242d3f
Natasha Libera <tash@artoftash.com>
2024-03-20 10:08:57 +0100

    Tweaked debug message on index page

--- app/gcmain/views.py
+++ app/gcmain/views.py
@@ -5,6 +5,6 @@ def mainAppView(request):
 
     # temp
     context = {};
-    context['django_server_message'] = 'This is a message from views.py  We are running the template using our django development server on port 8000.'
+    context['django_server_message'] = 'This is a message from views.py  We are running the template using our django on port 8000.'
 
     return render(request, 'gcmain/index.html', context)
