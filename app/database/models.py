ec3de46554a6d25864d132b5ae65878fbd505775
Natasha Libera <tash@artoftash.com>
2024-03-25 14:51:56 +0100

    Added the GCUser model

--- app/database/models.py
+++ app/database/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# User
+from django.contrib.auth.models import User
+
+# GCUser
+class GCUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
