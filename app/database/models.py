c297fa260c8a3c85eaeaf682fa66277bb69f8a9d
Natasha Libera <tash@artoftash.com>
2024-03-26 15:41:19 +0100

    Added Invitation model

--- app/database/models.py
+++ app/database/models.py
@@ -6,3 +6,9 @@ from django.contrib.auth.models import User
 # GCUser
 class GCUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+# Invitation
+class Invitation(models.Model):
+    email = models.CharField(max_length=255)
+    code = models.CharField(max_length=14)
+    date_sent = models.DateTimeField(auto_now_add=True)
