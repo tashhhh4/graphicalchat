d83774ed0b51b8b76e8f5de25c7d412c4aadecf9
Natasha Libera <tash@artoftash.com>
2024-03-27 16:23:49 +0100

    Refactored 3D Engine for multiple Screen definitions.

---
+++ app/game/Assets.js
@@ -0,0 +1,13 @@
+const ASSETS_URL = 'http://localhost:5173/assets/';
+
+const assets = {
+    models: {
+        BlueCube:       ASSETS_URL + 'sad_cube.glb',
+        RedCube:        ASSETS_URL + 'cute_cube_1.obj',
+        LargeFloor:     ASSETS_URL + 'large_square.glb',
+        StickWoman:     ASSETS_URL + 'stick_woman.glb',
+        IrregularFloor: ASSETS_URL + 'irregular_floor.glb',
+    }
+}
+
+export default assets;
--- app/game/game.js
+++ app/game/game.js
@@ -5,22 +5,22 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+/** Handwritten (later generated) list of all the objects in the game **/
+import assets from './Assets.js';
+
 // import { generateCollisionMap } from './generateCollisionMap.js';
 
-/** SETUP THE 3D ENVIRONMENT **/
-// Scene
-const scene = new THREE.Scene();
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-// Renderer
+/** GLOBAL **/
+// 3D Rendering Pieces
+let screen;
+let scene;
+let camera;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-
-// HTML5 Canvas Element
 const canvas = renderer.domElement;
-document.body.appendChild(canvas);
+
+// Current loading status & expected number of objects
+let loadingStatus = 0;
 
 // Entity Class
 class Entity {
@@ -69,202 +69,238 @@ class Velocity {
     static LEFTUP = 315;
 }
 
-/** Define 3D Models to be loaded into the scene **/
-// Scene Contents
-let sceneContents = [
-    new FloorEntity('Hub Floor', assets.models.IrregularFloor),
-    new Entity('Stick Woman', assets.models.StickWoman),
-];
+/** GAMEPLAY CONSTANTS **/
+const AVATAR_WALK_SPEED = .5;
 
-// Define which avatar is controlled by the user
-let myAvatar = findSceneContentsByName('Stick Woman');
+// Screen
+class Screen {
+    constructor() {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-// Get specific model helper function
-function findSceneContentsByName(name) {
-    for (const item of sceneContents) {
-        if (item.name === name) {
-            return item;
-        }
+        this.sceneIsSet = false;
+
+        // sceneContents is a list of Entity objects.
+        this.sceneContents = [];
+        this.findSceneContentsByName = (name) => {
+            for (const entity of this.sceneContents) {
+                if (entity.name === name) {
+                    return entity;
+                }
+            }
+        };
+        this.loadSceneContents = () => {
+
+            // Reset global loading progress
+            loadingStatus = 0;
+
+            let countFiles = 0;
+            const totalFiles = this.sceneContents.length;
+
+            const scene = this.scene;
+
+            for (const entity of this.sceneContents) {
+                const loader = new GLTFLoader();
+                loader.load(
+
+                    // Resource URL
+                    entity.file,
+
+                    // onLoad : Function
+                    function(gltf) {
+
+                        // Define Entity Object
+                        const model = gltf.scene.children[0];
+                        model.name = entity.name;
+                        entity.model = model;
+                        // const map = generateCollisionMap(model);
+                        // entity.collisionMap = map; 
+
+                        // Add to scene
+                        scene.add(model);
+                        countFiles += 1;
+                        loadingStatus = countFiles / totalFiles * 100;
+                        //this.objectsLoaded = countFiles / this.totalFiles * 100;
+                        console.log('Object Loading Progress: ' + loadingStatus + '%');
+                    },
+                
+                    // onProgress : Function
+                    undefined,
+
+                    // onError : Function
+                    undefined
+                );
+            }
+        };
+        this.updateEntityPositions = () => {};
+        this.run = () => {
+            if (loadingStatus === 100) {
+                requestAnimationFrame(this.run);
+        
+                // Manual adjustment of object position after loading
+                // unnecessary - use data to update entity pos when loading
+                if (this.sceneIsSet == false) {
+                    this.sceneIsSet = true;
+                }
+        
+                // Update entity positions
+                this.updateEntityPositions();
+        
+                renderer.render(scene, camera);
+            } else {
+                setTimeout(this.run, 100);
+            }
+        };
     }
 }
 
-// Loaded State (0/100)
-let objectsLoaded = 0;
+function changeScreen(newScreen) {
+    screen = newScreen;
+    camera = newScreen.camera;
+    scene = newScreen.scene;
+    screen.loadSceneContents();
+    screen.initControls();
+    screen.run();
+}
 
-// Moved objects around after loading them
-let sceneIsSet = false;
 
-// Keep track of held-down keys
-const pressedKeys = {};
+// Main app mode, Game & Chatroom
+class GameScreen extends Screen {
+    constructor() {
+        super();
 
-// Gameplay constants
-const AVATAR_WALK_SPEED = .5;
+        // List of Entity objects to load
+        this.sceneContents = [
+            new FloorEntity('Hub Floor', assets.models.IrregularFloor),
+            new Entity('Stick Woman', assets.models.StickWoman),
+        ];
 
+        // Player character
+        this.myAvatar = this.findSceneContentsByName('Stick Woman');
+
+        // Lighting
+        this.scene.add(new THREE.AmbientLight(0xfefefe));
+        this.scene.add(new THREE.DirectionalLight(0xffffee, .8));
+
+        // Camera
+        this.camera.position.z = 16;
+        this.camera.position.y = 6;
+        this.camera.position.x = 0;
+        this.camera.rotateX(-0.5);
+
+        // Define animation behavior
+        this.updateEntityPositions = () => {
+            this.myAvatar.move();
+        }
+
+        // Runs when active 'screen' is changed to update window controls
+        this.initControls = () => {
+            const handleKeyDown = (event) => {
+
+                // Pressed Keys
+                pressedKeys[event.keyCode] = true;
+            
+                // Velocity
+                const keys = Object.keys(pressedKeys);
+                if (keys.length === 1) {
+            
+                    const k = keys[0];
+                    if (k == ARROW_UP) {
+                        this.myAvatar.velocity.speed = AVATAR_WALK_SPEED;
+                        this.myAvatar.velocity.angle = Velocity.UP;
+                    } else
+                    if (k == ARROW_RIGHT) {
+                        this.myAvatar.velocity.speed = AVATAR_WALK_SPEED;
+                        this.myAvatar.velocity.angle = Velocity.RIGHT;
+                    } else
+                    if (k == ARROW_DOWN) {
+                        this.myAvatar.velocity.speed = AVATAR_WALK_SPEED;
+                        this.myAvatar.velocity.angle = Velocity.DOWN;
+                    } else
+                    if (k == ARROW_LEFT) {
+                        this.myAvatar.velocity.speed = AVATAR_WALK_SPEED;
+                        this.myAvatar.velocity.angle = Velocity.LEFT;
+                    }
+            
+                } else if (keys.length === 2) {
+                    const k1 = keys[0];
+                    const k2 = keys[1];
+            
+                    // UP-RIGHT
+                    if ((k1 == ARROW_UP && k2 == ARROW_RIGHT) || (k1 == ARROW_RIGHT && k2 == ARROW_UP)) {
+                        this.myAvatar.velocity.speed = AVATAR_WALK_SPEED;
+                        this.myAvatar.velocity.angle = Velocity.UPRIGHT;
+                    }
+                    // RIGHT-DOWN
+                    if ((k1 == ARROW_RIGHT && k2 == ARROW_DOWN) || (k1 == ARROW_DOWN && k2 == ARROW_RIGHT)) {
+                        this.myAvatar.velocity.speed = AVATAR_WALK_SPEED;
+                        this.myAvatar.velocity.angle = Velocity.RIGHTDOWN;
+                    }
+                    // DOWN-LEFT
+                    if ((k1 == ARROW_DOWN && k2 == ARROW_LEFT) || (k1 == ARROW_LEFT && k2 == ARROW_DOWN)) {
+                        this.myAvatar.velocity.speed = AVATAR_WALK_SPEED;
+                        this.myAvatar.velocity.angle = Velocity.DOWNLEFT;
+                    }
+                    // LEFT-UP
+                    if ((k1 == ARROW_LEFT && k2 == ARROW_UP) || (k1 == ARROW_UP && k2 == ARROW_LEFT)) {
+                        this.myAvatar.velocity.speed = AVATAR_WALK_SPEED;
+                        this.myAvatar.velocity.angle = Velocity.LEFTUP;
+                    }
+                }
+            }
+        
+            const handleKeyUp = (event) => {
+                // Pressed Keys
+                delete pressedKeys[event.keyCode];
+                
+                // Velocity
+                this.myAvatar.velocity.speed = 0;
+            }
+            document.addEventListener('keydown', handleKeyDown);
+            document.addEventListener('keyup', handleKeyUp);
+        };
+    }    
+}
+
+// Sets the user up for customizing their avatar
+class AvatarEditScreen extends Screen {
+    constructor() {
+        super();
+    }
+}
+
+// Sets the user up for customizing their hub
+class HubEditScreen extends Screen {
+    constructor() {
+        super();
+    }
+}
+
+const gameScreen = new GameScreen();
+const avatarEditScreen = new AvatarEditScreen();
+const hubEditScreen = new HubEditScreen();
+
+
+/** UNIVERSAL WINDOW & KEYBOARD SETTINGS **/
 // Keyboard key names
 const ARROW_UP = 38;
 const ARROW_DOWN = 40;
 const ARROW_LEFT = 37;
 const ARROW_RIGHT = 39;
 
-// Content (Geometry, Material, Mesh, Loaders, Light)
-function loadSceneContents() {
-    const totalFiles = sceneContents.length;
-    let countFiles = 0;
-
-    for (const item of sceneContents) {
-        const loader = new GLTFLoader();
-        loader.load(
-
-            // Resource URL
-            item.file,
-
-            // onLoad : Function
-            function(gltf) {
-                
-                // Define Entity Object
-                const model = gltf.scene.children[0];
-                model.name = item.name;
-                item.model = model;
-                // const map = generateCollisionMap(model);
-                // item.collisionMap = map; 
-
-                // Track additions
-                scene.add(model);
-                countFiles += 1;
-                objectsLoaded = countFiles / totalFiles * 100;
-                console.log('Object Loading Progress: ', objectsLoaded, '%');
-            },
-            
-            // onProgress : Function
-            undefined,
-
-            // onError : Function
-            undefined
-        );
-    }
-}
-
-
-// Lighting
-const ambient_light = new THREE.AmbientLight(0xfefefe);
-const directional_light = new THREE.DirectionalLight(0xffffee, .8);
-
-// Set up the scene
-//scene.add(cube_3);
-scene.add(ambient_light);
-scene.add(directional_light);
-
-// That will do for now.
-camera.position.z = 16;
-camera.position.y = 6;
-camera.position.x = 0;
-camera.rotateX(-0.5);
-const stickWomanPosZ = 10;
-
-
-/** User Input Controls **/
-// Keyboard Controls
-document.addEventListener('keydown', handleKeyDown);
-document.addEventListener('keyup', handleKeyUp);
-
-function handleKeyDown(event) {
-    // Pressed Keys
-    pressedKeys[event.keyCode] = true;
-
-    // Velocity
-    const keys = Object.keys(pressedKeys);
-    if (keys.length === 1) {
-
-        const k = keys[0];
-        if (k == ARROW_UP) {
-            myAvatar.velocity.speed = AVATAR_WALK_SPEED;
-            myAvatar.velocity.angle = Velocity.UP;
-        } else
-        if (k == ARROW_RIGHT) {
-            myAvatar.velocity.speed = AVATAR_WALK_SPEED;
-            myAvatar.velocity.angle = Velocity.RIGHT;
-        } else
-        if (k == ARROW_DOWN) {
-            myAvatar.velocity.speed = AVATAR_WALK_SPEED;
-            myAvatar.velocity.angle = Velocity.DOWN;
-        } else
-        if (k == ARROW_LEFT) {
-            myAvatar.velocity.speed = AVATAR_WALK_SPEED;
-            myAvatar.velocity.angle = Velocity.LEFT;
-        }
-
-    } else if (keys.length === 2) {
-        const k1 = keys[0];
-        const k2 = keys[1];
-
-        // UP-RIGHT
-        if ((k1 == ARROW_UP && k2 == ARROW_RIGHT) || (k1 == ARROW_RIGHT && k2 == ARROW_UP)) {
-            myAvatar.velocity.speed = AVATAR_WALK_SPEED;
-            myAvatar.velocity.angle = Velocity.UPRIGHT;
-        }
-        // RIGHT-DOWN
-        if ((k1 == ARROW_RIGHT && k2 == ARROW_DOWN) || (k1 == ARROW_DOWN && k2 == ARROW_RIGHT)) {
-            myAvatar.velocity.speed = AVATAR_WALK_SPEED;
-            myAvatar.velocity.angle = Velocity.RIGHTDOWN;
-        }
-        // DOWN-LEFT
-        if ((k1 == ARROW_DOWN && k2 == ARROW_LEFT) || (k1 == ARROW_LEFT && k2 == ARROW_DOWN)) {
-            myAvatar.velocity.speed = AVATAR_WALK_SPEED;
-            myAvatar.velocity.angle = Velocity.DOWNLEFT;
-        }
-        // LEFT-UP
-        if ((k1 == ARROW_LEFT && k2 == ARROW_UP) || (k1 == ARROW_UP && k2 == ARROW_LEFT)) {
-            myAvatar.velocity.speed = AVATAR_WALK_SPEED;
-            myAvatar.velocity.angle = Velocity.LEFTUP;
-        }
-    }
-}
-
-function handleKeyUp(event) {
-    // Pressed Keys
-    delete pressedKeys[event.keyCode];
-    
-    // Velocity
-    myAvatar.velocity.speed = 0;
-}
-
-// Other Window Events
 window.addEventListener('resize', function(){
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 });
 
-// Gameplay & animation logic functions
-function updateEntityPositions() {
-    // TODO: list of Entities, go through each entity and detect changes?
-    const avatar = findSceneContentsByName("Stick Woman");
+const pressedKeys = {};
 
-    avatar.move();
-}
-
-
-// Process logic and render the scene
-function run() {
-    if (objectsLoaded === 100) {
-        requestAnimationFrame(run);
-
-        // Manual adjustment of object position after loading
-        if (sceneIsSet == false) {
-            const avatar = findSceneContentsByName("Stick Woman");
-            sceneIsSet = true;
-        }
-
-        // Update entity positions
-        updateEntityPositions();
-
-        renderer.render(scene, camera);
-    } else {
-        //console.log('The files are not loaded yet...');
-        setTimeout(run, 100);
-    }
-}
 
 /** PAGE INIT **/
-loadSceneContents();
-run();
+
+// HTML5 Canvas Element
+document.body.appendChild(canvas);
+
+// Pick screen
+changeScreen(gameScreen);
