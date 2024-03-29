029cdfc45bee199a059800f0b97c58d21a785548
Natasha Libera <tash@artoftash.com>
2024-03-29 11:20:04 +0100

    Fixed html import for UI Test

--- app/game/ui_test.js
+++ app/game/ui_test.js
@@ -1,8 +1,7 @@
 console.log('Loaded ui_test.js');
 
 // Import HTML
-// import htmlTemplate from './test_template.html';
-const html = '<div class="ui-textbox">A cute text box appeared!</div>'
+import html from './test_template.html?raw';
 
 // Import CSS
 import './test_ui.css';
