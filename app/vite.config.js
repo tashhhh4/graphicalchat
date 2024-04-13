c65f41c7abc3d662905c5707876f6313f580491a
Natasha Libera <tash@artoftash.com>
2024-04-13 15:21:41 -0500

    Reinstalled and moved js dependencies around.

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: '../',
     build: {
         outDir: 'gcmain/static/game/main',
         assetsDir: '.',
         rollupOptions: {
             input: {
-                bundle: '../index.html'
+                bundle: 'index.html'
             },
             output: {
                 entryFileNames: '[name].js'
